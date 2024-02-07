37d51b59fe350ff66e617b982260ad1378c7cb68
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-02-07 15:48:26 +0100

    chore: update naming scheme

--- src/overlays/snapshot.nix
+++ src/overlays/snapshot.nix
@@ -9,10 +9,10 @@ let
   makePhpPackage = import ../makePhpPackage.nix inputs final prev;
 
   branches = {
-    php-8-1-snapshot = { version = "8.1.999.${inputs.php-src-81.shortRev}"; src = inputs.php-src-81; };
-    php-8-2-snapshot = { version = "8.2.999.${inputs.php-src-82.shortRev}"; src = inputs.php-src-82; };
-    php-8-3-snapshot = { version = "8.3.999.${inputs.php-src-83.shortRev}"; src = inputs.php-src-83; };
-    php-master-snapshot = { version = "8.4.999.${inputs.php-src-master.shortRev}"; src = inputs.php-src-master; };
+    php-8-1-snapshot = { version = "8.1.999-${inputs.php-src-81.shortRev}"; src = inputs.php-src-81; };
+    php-8-2-snapshot = { version = "8.2.999-${inputs.php-src-82.shortRev}"; src = inputs.php-src-82; };
+    php-8-3-snapshot = { version = "8.3.999-${inputs.php-src-83.shortRev}"; src = inputs.php-src-83; };
+    php-master-snapshot = { version = "8.4.999-${inputs.php-src-master.shortRev}"; src = inputs.php-src-master; };
   };
 in
 lib.mapAttrs
