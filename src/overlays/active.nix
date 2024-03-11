dc4747faf5267b8f94b92f76213aa82392d72331
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-03-11 12:37:17 +0100

    fix: add more patches

--- src/overlays/active.nix
+++ src/overlays/active.nix
@@ -6,6 +6,8 @@ prev:
 let
   inherit (prev) lib;
 
+  patches = prev.callPackage ../patches.nix { };
+
   makePhpPackage = import ../makePhpPackage.nix inputs final prev;
 
   makePackageSet = versions: lib.foldl'
@@ -26,20 +28,11 @@ let
     { }
     versions;
 
-  versions =
-    let
-      libxmlpatch =
-        (prev.fetchpatch {
-          url = "https://github.com/php/php-src/commit/0a39890c967aa57225bb6bdf4821aff7a3a3c082.patch";
-          hash = "sha256-HvpTL7aXO9gr4glFdhqUWQPrG8TYTlvbNINq33M3zS0=";
-        })
-      ;
-    in
-    [
-      { version = "8.1.26"; hash = "sha256-g73iSchKoaBDqMjQ7qCTRcLK5puXhM3wIin8kW+7nqA="; patches = [ libxmlpatch ]; }
-      { version = "8.2.13"; hash = "sha256-ZlKfQ7ITEx5rJTxWAr7wXwSUWNISknMPzNY7SKBtZ7o="; }
-      { version = "8.3.0"; hash = "sha256-3mfQgz1CsZblpm+hozL0Xilsvo6UcuklayoHHDTcXtY="; }
-    ];
+  versions = [
+    { version = "8.1.26"; hash = "sha256-g73iSchKoaBDqMjQ7qCTRcLK5puXhM3wIin8kW+7nqA="; patches = [ patches.libxmlpatch patches.ext_sqlite3 ]; }
+    { version = "8.2.13"; hash = "sha256-ZlKfQ7ITEx5rJTxWAr7wXwSUWNISknMPzNY7SKBtZ7o="; }
+    { version = "8.3.0"; hash = "sha256-3mfQgz1CsZblpm+hozL0Xilsvo6UcuklayoHHDTcXtY="; }
+  ];
 in
 lib.mapAttrs
   (k: v: (makePhpPackage v))
---
+++ src/patches.nix
@@ -0,0 +1,13 @@
+{
+  fetchpatch
+}:
+{
+  libxmlpatch = (fetchpatch {
+    url = "https://github.com/php/php-src/commit/0a39890c967aa57225bb6bdf4821aff7a3a3c082.patch";
+    hash = "sha256-HvpTL7aXO9gr4glFdhqUWQPrG8TYTlvbNINq33M3zS0=";
+  });
+  ext_sqlite3 = (fetchpatch {
+    url = "https://raw.githubusercontent.com/NixOS/nixpkgs/master/pkgs/development/interpreters/php/skip-sqlite3_bind_bug68849.phpt.patch";
+    hash = "sha256-D4UeUTHlSVJqcHiyj7smvhPnOyV51S2OloWcUmRWwJY=";
+  });
+}
