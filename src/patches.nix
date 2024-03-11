ff07b06a033744ee33c57dc57cd5b2b13de9284a
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-03-11 15:03:23 +0100

    fix: add more patches

--- src/patches.nix
+++ src/patches.nix
@@ -9,6 +9,14 @@
     url = "https://raw.githubusercontent.com/NixOS/nixpkgs/master/pkgs/development/interpreters/php/skip-sqlite3_bind_bug68849.phpt.patch";
     hash = "sha256-D4UeUTHlSVJqcHiyj7smvhPnOyV51S2OloWcUmRWwJY=";
   });
+  # https://github.com/php/php-src/commit/2a4775d6a73e9f6d4fc8e7df6f052aa18790a8e9
+  ext_sqlite3_tests = (fetchpatch {
+    url = "https://github.com/php/php-src/commit/2a4775d6a73e9f6d4fc8e7df6f052aa18790a8e9.patch";
+    hash = "sha256-2VNfURGZmIEXtoLxOLX5wec9mqNGEWPY3ofCMw4E7S0=";
+    excludes = [
+      "NEWS"
+    ];
+  });
   ext_dom = (fetchpatch {
     url = "https://github.com/php/php-src/commit/061058a9b1bbd90d27d97d79aebcf2b5029767b0.patch";
     hash = "sha256-0hOlAG+pOYp/gUU0MUMZvzWpgr0ncJi5GB8IeNxxyEU=";
